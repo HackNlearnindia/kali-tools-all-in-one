a356f31f1cdf9f5841d111282c9bd8659226589b
HackNLearn <65217159+HackNlearnindia@users.noreply.github.com>
2026-01-16 16:59:21 +0530

    Delete core directory

--- core/github_auto_runner.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-xdg-open https://github.com/digininja/DVWA
-xdg-open https://github.com/OWASP/WebGoat
-xdg-open https://github.com/vulhub/vulhub
--- core/update_tools.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Updating Kali Linux tools..."
-sudo apt update
-sudo apt upgrade -y
-echo "Update completed"
